b710b46b7e05d76c97255d5c78a71add641b8ff8
SoorajTharumia <sendsooraj@gmail.com>
2025-01-30 16:14:32 -0500

    committed .env

--- firebase/config.js
+++ firebase/config.js
@@ -7,29 +7,13 @@ import { getFirestore, setDoc, doc } from "firebase/firestore";
 
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  // gamingwizard account details
-  
-  apiKey: "AIzaSyD8fqUwHG725Gq5PrO4WBQ4in5BV7okPdM",
-  authDomain: "parklandcommunityapp-7d8d2.firebaseapp.com",
-  projectId: "parklandcommunityapp-7d8d2",
-  storageBucket: "parklandcommunityapp-7d8d2.appspot.com",
-  messagingSenderId: "64218480246",
-  appId: "1:64218480246:web:7275b71a42f1e91bb8e957",
-
-
-
-
-
-
-
-  // sendsooraj account details
-  // apiKey: "AIzaSyADREIX2CHnAp-h2dgQO5R4CTm-Zhpthxk",
-  // authDomain: "parklandcommunityapp.firebaseapp.com",
-  // projectId: "parklandcommunityapp",
-  // storageBucket: "parklandcommunityapp.appspot.com",
-  // messagingSenderId: "31839694854",
-  // appId: "1:31839694854:web:3b9d78f51543dfc40d5387",
+const firebaseConfig = {  
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 if (!firebase.apps.length) {
